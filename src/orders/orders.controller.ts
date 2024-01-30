a04764b77bbc1ba6a0c2de61ab13148e2e187151
cody proo <cody20v@gmail.com>
2024-01-30 15:32:25 +0330

    x

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -17,6 +17,7 @@ import { Categories } from 'src/categories/categories.entity';
 import { In } from 'typeorm';
 import { Orders } from './orders.entity';
 import { MultipleSaveOrderDto } from './dtos/multiple-save-order.dto';
+import * as moment from 'moment';
 
 @Controller('orders')
 export class OrdersController {
@@ -123,19 +124,15 @@ export class OrdersController {
     return order.save();
   }
 
-  @Get('own/page')
+  @Get('own')
   async getOwnOrder(@Query() query: any) {
     const therapist = await Therapist.findOne({ where: {} });
     let where: Record<any, any> = {
       therapist: { id: therapist.id },
     };
     if (query.status) where['status'] = query.status;
-    const limit = +(query['limit'] || 10);
-    const page = +(query['page'] || 0) * limit;
     const content = await Orders.find({
       order: { id: -1 },
-      skip: page,
-      take: limit,
       where,
       relations: {
         therapist: true,
@@ -143,9 +140,89 @@ export class OrdersController {
         documentation: true,
       },
     });
-    const count = await Orders.count({
-      where,
+    return content;
+  }
+
+  @Get('detail/:therapistId')
+  async getOrderObjects(@Param('therapistId') therapistId: number) {
+    const orders = await Orders.find({
+      order: { id: -1 },
+      where: { therapist: { id: therapistId } },
+      relations: {
+        patient: true,
+      },
     });
-    return { content, count };
+    const patients = orders
+      .map((e) => e.patient)
+      .reduce((acc, item) => {
+        if (!acc.find((element) => element.id === item.id)) acc.push(item);
+        return acc;
+      }, []);
+
+    const locations = orders
+      .map((e) => e.address)
+      .reduce((acc, item) => {
+        if (!acc.includes(item)) acc.push(item);
+        return acc;
+      }, []);
+
+    const times = orders
+      .map((e) => `${e.startHour}_${e.endHour}`)
+      .reduce((acc, item) => {
+        if (!acc.includes(item)) acc.push(item);
+        return acc;
+      }, []);
+
+    const categories = orders
+      .map((e) => e.categories)
+      .reduce((acc, item) => {
+        item.map((i) => acc.push(i));
+        return acc;
+      }, [])
+      .reduce((acc, item) => {
+        if (!acc.find((e) => e.enName === item.enName)) acc.push(item);
+        return acc;
+      }, []);
+
+    return { patients, locations, times, categories };
+  }
+
+  @Get('today/:therapistId')
+  getTodayOrder(@Param('therapistId') id: number) {
+    console.log(new Date().toLocaleDateString());
+    return Orders.find({
+      where: { date: moment().format('YYYY-MM-DD') as any, therapist: { id } },
+      order: { id: -1 },
+      relations: {
+        therapist: true,
+        patient: true,
+        documentation: true,
+      },
+    });
+  }
+
+  @Get('patient/:patientId')
+  getOrderByPatientId(@Param('patientId') id: number) {
+    return Orders.find({
+      where: { patient: { id } },
+      relations: {
+        documentation: { order: { therapist: true } },
+        therapist: true,
+        patient: true,
+      },
+      order: { id: -1 },
+    });
+  }
+
+  @Get(':id')
+  getOrderById(@Param('id') id: number) {
+    return Orders.findOne({
+      where: { id },
+      relations: {
+        documentation: { order: { therapist: true } },
+        therapist: true,
+        patient: true,
+      },
+    });
   }
 }
