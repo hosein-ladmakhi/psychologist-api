f3dbf6c0b6663f558867c082d1518a9e40476a66
cody proo <cody20v@gmail.com>
2024-03-09 11:37:52 +0330

    X

--- src/therapist-schedules/therapist-schedules.controller.ts
+++ src/therapist-schedules/therapist-schedules.controller.ts
@@ -107,7 +107,7 @@ export class TherapistSchedulesController {
   async getTherapistSchedulesPerDay(@Param('id', ParseIntPipe) id: number) {
     const datas = await TherapistSchedules.find({
       where: { therapist: { id } },
-      relations: { location: true },
+      relations: { location: true, therapist: true },
     });
     const days = [
       {
